0992dd2659ba963d0a444081933ac958344e6324
Milan Jeličič <mjelicic@nil.com>
2016-11-06 22:53:43 +0100

    Moving items from list1 to list2 works

--- app.js
+++ app.js
@@ -11,21 +11,39 @@ function ToBuyController(ShoppingListCheckOffService) {
   var shoppingList = this;
 
   shoppingList.items = ShoppingListCheckOffService.getItems();
+  shoppingList.moveToBought = function (index) {
+    console.log("topindex: " + index);
+    ShoppingListCheckOffService.moveToBought(index);
+  }
+
 }
 
 AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
 function AlreadyBoughtController(ShoppingListCheckOffService) {
+  var boughtList = this;
+
+  boughtList.items = ShoppingListCheckOffService.getBoughtItems();
+
 }
 
 function ShoppingListCheckOffService() {
   var service = this;
-  var tobuy = ['cokies', 'chocolate', 'candy', 'cake', 'doughnuts', 'pie'];
+  var tobuy = ['cookies', 'chocolate', 'candy', 'cake', 'doughnuts', 'pie'];
   var bought = [];
 
   service.getItems = function () {
     return tobuy;
   }
 
+  service.getBoughtItems = function () {
+    return bought;
+  }
+
+  service.moveToBought = function (index) {
+    console.log("index: ", index);
+    bought.push(tobuy[index]);
+    tobuy.splice(index, 1);
+  }
 }
 
 })();
